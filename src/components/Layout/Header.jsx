0133b5639422e9260cb74e724ee07d7a90e55156
Nurullo <khuwaidullaev@gmail.com>
2021-06-08 06:44:53 +0500

    Added the Cart Button

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,15 +1,16 @@
 import React, { Fragment } from "react";
 import classes from "./Header.module.css";
 import mealsImg from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCartButton />
       </header>
-      {/* since we our using dash that's why we can't use .notation*/}
+      {/* since we our using -dash, that's why we can't use .notation*/}
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="Table of foods" />
       </div>
